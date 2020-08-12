f0a3018ac6257e60e519b5926dace3b263115e96
aretask2@gmail.com <aretask2@gmail.com>
2020-08-12 18:48:12 +0300

    Add exact property to a button for active styling

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import classes from './NavigationItems.module.css'
 
 const navigationItems = ( props ) => (
     <div className={classes.NavigationItems}>
-        <Button clicked={props.clicked} link='/'>ABOUT ME</Button>
+        <Button clicked={props.clicked} link='/' exact>ABOUT ME</Button>
         <Button clicked={props.clicked} link='/personal_skills'>PERSONAL SKILLS</Button>
         <Button clicked={props.clicked} link='/gallery'>GALLERY</Button>
     </div>
