524a952a44be5eb821a0e31de204b07f0be59aa2
aretask2@gmail.com <aretask2@gmail.com>
2020-08-14 11:34:50 +0300

    Fix button bug

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,9 +4,10 @@ import { NavLink } from 'react-router-dom';
 import classes from './Button.module.css';
 
 const button = ( props ) => (
-    <button className={classes.Button} onClick={props.clicked}>
+    <button className={classes.Button} >
         <div className={classes.Circle}></div>
         <NavLink 
+            onClick={props.clicked}
             to={props.link} 
             exact={props.exact}
             activeClassName={classes.active}>{props.children}</NavLink>
