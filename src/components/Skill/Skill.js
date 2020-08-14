55e323505ded6c4fa7949b59516e43ec2dbea1a7
aretask2@gmail.com <aretask2@gmail.com>
2020-08-14 11:18:23 +0300

    Add skill component with full stylying

---
+++ src/components/Skill/Skill.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import classes from './Skill.module.css'
+import Logo from '../Logo/Logo'
+
+const skill = ( props ) => (
+    <div className={classes.Skill}>
+        <div className={classes.SkillHeader}>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <div className={classes.SkillName}>
+                <h3>{props.name}</h3>
+            </div>
+        </div>
+        <div className={classes.Descripion}>
+            <p>{props.description}</p>
+        </div>
+    </div>   
+)
+
+export default skill;
