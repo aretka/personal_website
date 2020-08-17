29e1d9baec03873f8de0e631b702b1c430231988
aretask2@gmail.com <aretask2@gmail.com>
2020-08-17 17:02:35 +0300

    Rename navigation button

---
+++ src/components/UI/NavButton/NavButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import classes from './NavButton.module.css';
+
+const button = ( props ) => (
+    <button className={classes.Button} >
+        <div className={classes.Circle}></div>
+        <NavLink 
+            onClick={props.clicked}
+            to={props.link} 
+            exact={props.exact}
+            activeClassName={classes.active}>{props.children}</NavLink>
+    </button>
+) 
+
+export default button;
