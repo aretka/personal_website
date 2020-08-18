4d0cb51bd94da43e043e7b1b2bb961857f0a43ee
aretask2@gmail.com <aretask2@gmail.com>
2020-08-18 11:10:59 +0300

    Add validation

--- src/components/Skill/AddSkillArea/AddSkillArea.js
+++ src/components/Skill/AddSkillArea/AddSkillArea.js
@@ -8,11 +8,51 @@ class AddSkillArea extends Component {
         skill: {
             name: {
                 value: '',
+                validation: {
+                    required: true
+                },
+                touched: false,
+                valid: false
             },
             description: {
-
+                value: '',
+                validation: {
+                    required: true
+                },
+                touched: false,
+                valid: false
             }
+        },
+        formIsValid: false
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
         }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedInputs = {
+            ...this.state.skill
+        };
+        const updatedInputElement = {
+            ...updatedInputs[inputIdentifier]
+        };
+        updatedInputElement.value = event.target.value;
+        updatedInputElement.valid = this.checkValidity(updatedInputElement.value, updatedInputElement.validation);
+        updatedInputElement.touched = true;
+        updatedInputs[inputIdentifier] = updatedInputElement;
+        
+        let formIsValid = true;
+        for (let inputIdentifier in updatedInputs) {
+            formIsValid = updatedInputs[inputIdentifier].valid && formIsValid
+        }
+        this.setState({skill: updatedInputs, formIsValid: formIsValid})
     }
 
     render () {
@@ -23,14 +63,17 @@ class AddSkillArea extends Component {
                     <input 
                         className={classes.Input}
                         type='text'
-                        placeholder='Skill name'>
+                        placeholder='Skill name'
+                        onChange={(event) => this.inputChangedHandler(event, 'name')}>
                     </input>
                     <input 
                         className={classes.Input}
                         type='text'
-                        placeholder='Skill description'>
+                        placeholder='Skill description'
+                        onChange={(event) => this.inputChangedHandler(event, 'description')}>
                     </input>
                 </form>
+                <p>{this.state.skill.name.value}</p>
                 <ModalButton btnType="Danger" clicked={this.props.onCancelAddingSkill}>Cancel</ModalButton>
                 <ModalButton btnType="Success">Continue</ModalButton>
             </>
