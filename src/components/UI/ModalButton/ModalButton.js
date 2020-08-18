0c17698f87c9ab902f8fee1fe7d90058cb661fd8
aretask2@gmail.com <aretask2@gmail.com>
2020-08-18 12:40:31 +0300

    Add type and disabled properties to button

--- src/components/UI/ModalButton/ModalButton.js
+++ src/components/UI/ModalButton/ModalButton.js
@@ -5,7 +5,9 @@ import classes from './ModalButton.module.css'
 const modalButton = ( props ) => (
     <button
         className={[classes.Button, classes[props.btnType]].join(' ')}
-        onClick={props.clicked}>{props.children}
+        onClick={props.clicked}
+        disabled={props.disabled}
+        type={props.type}>{props.children}
     </button>
 )
 
