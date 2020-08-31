acec75e4d467783800e819a397ac608eae1f1720
aretask2@gmail.com <aretask2@gmail.com>
2020-08-31 13:46:05 +0300

    Import dynamic stylying

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,7 +7,12 @@ const modal = ( props ) =>  {
     return (
         <>
             <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className={classes.Modal}>
+            <div 
+                className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
                 {props.children}
             </div>
         </>
