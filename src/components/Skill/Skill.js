76cd482f3a416a28cb24c2a540a6fad8351462cb
aretask2@gmail.com <aretask2@gmail.com>
2020-08-31 14:03:46 +0300

    Add remove logo

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import classes from './Skill.module.css'
 import Logo from '../Logo/Logo'
+import removeLogo from '../../images/removeLogo.png'
 
 const skill = ( props ) => (
     <div className={classes.Skill}>
-        <div className={classes.SkillHeader}>
-            <div className={classes.Logo}>
-                <Logo logo={props.name}/>
+        <div className={classes.Container}>
+            <div className={classes.SkillHeader}>
+                <div className={classes.Logo}>
+                    <Logo logo={props.name}/>
+                </div>
+                <div className={classes.SkillName}>
+                    <h3>{props.name}</h3>
+                </div>
             </div>
-            <div className={classes.SkillName}>
-                <h3>{props.name}</h3>
+            <div className={classes.RemoveLogo}>
+                <div className={classes.Logo}>
+                    <img onClick={props.clicked} className={classes.ButtonHover} src={removeLogo} alt='RemoveLogo'/>
+                </div>
             </div>
         </div>
         <div className={classes.Descripion}>
             <p>{props.description}</p>
         </div>
-    </div>   
+    </div>
 )
-
+             
 export default skill;
