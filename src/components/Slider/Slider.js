62e6b1886a54ecc3d2429144bd132f3e0c19ef8d
aretask2@gmail.com <aretask2@gmail.com>
2020-09-01 09:58:55 +0300

    Create slider component

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+
+import cssLogo from '../../images/cssLogo.png'
+import reactLogo from '../../images/reactLogo.png'
+import classes from './Gallery.module.css'
+
+const slider = () => (
+    <div className={classes.Slider}>
+        <Carousel showArrows={true} showThumbs={false} showStatus={false} autoPlay={true}>
+            <div>
+                <img src={cssLogo} alt="pic"></img>
+            </div>
+            <div>
+                <img src={reactLogo} alt="pic"></img>
+            </div>
+        </Carousel>
+    </div>
+)
+
+export default slider;
